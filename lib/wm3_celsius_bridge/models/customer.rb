6ea2bfd54dd7072acb95232651a357ed8b7e1597
Tobias Lindholm <tobias.lindholm@antob.se>
2017-12-11 16:07:32 +0100

    Update customer model.

--- lib/wm3_celsius_bridge/models/customer.rb
+++ lib/wm3_celsius_bridge/models/customer.rb
@@ -4,28 +4,28 @@ module Wm3CelsiusBridge
   # The Customer Class wraps the SOAP
   # response into something simpler.
   class Customer < Dry::Struct
+    # General
     attribute :no, Types::NonBlankStrippedString
     attribute :name, Types::StrippedString
     attribute :address, Types::StrippedString
     attribute :post_code, Types::StrippedString
     attribute :city, Types::StrippedString
-    attribute :phone_no, Types::Strict::Array.of(Types::StrippedString)
-    attribute :fax_no, Types::StrippedString
 
-    attribute :balance_lcy, Types::CustomFloat
+    # Communication
+    attribute :phone_no, Types::StrippedString
+
+    # Other (not verified)
     attribute :last_date_modified, Types::CustomDate
-
-    attribute :copy_sellto_addrto_qte_from, Types::StrippedString
-
-    attribute :gen_bus_posting_group, Types::Coercible::String
-    attribute :vat_bus_posting_group, Types::Coercible::String
-    attribute :customer_posting_group, Types::Coercible::String
-    attribute :customer_price_group, Types::Coercible::String
-
-    attribute :allow_line_disc, Types::Strict::Bool
-    attribute :application_method, Types::Coercible::String
-
-    attribute :payment_terms_code, Types::Coercible::String
-    attribute :inder_terms_code, Types::Coercible::String
+    # attribute :fax_no, Types::StrippedString
+    # attribute :balance_lcy, Types::CustomFloat
+    # attribute :copy_sellto_addrto_qte_from, Types::StrippedString
+    # attribute :gen_bus_posting_group, Types::Coercible::String
+    # attribute :vat_bus_posting_group, Types::Coercible::String
+    # attribute :customer_posting_group, Types::Coercible::String
+    # attribute :customer_price_group, Types::Coercible::String
+    # attribute :allow_line_disc, Types::Strict::Bool
+    # attribute :application_method, Types::Coercible::String
+    # attribute :payment_terms_code, Types::Coercible::String
+    # attribute :inder_terms_code, Types::Coercible::String
   end
 end
